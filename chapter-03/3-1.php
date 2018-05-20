604ba779d3e27f203c2cdc1ebbbb60bbc481ca94
ueki05 <uekiyuta1991@gmail.com>
2018-05-20 21:03:05 +0900

    3.1.4 文字列

--- chapter-03/3-1.php
+++ chapter-03/3-1.php
@@ -35,3 +35,41 @@
     var_dump($int2);
 ?>
 </ul>
+
+<h2>3.1.4 文字列</h2>
+<?php
+  $string1 = 'this is string';
+  $string2 = "this is string";
+  $string3 = 'hi, $string2 \n'; // シングルクォートは変数展開されない
+  $string4 = "hi, $string2 \n"; // ダブルクォートは変数展開される
+  var_dump($string1);
+  var_dump($string2);
+  var_dump($string3);
+  var_dump($string4);
+
+  $age = 15;
+
+  // $ageというヘンスだが、どこまでが変数だかわからない
+  echo "$ageyears old.", PHP_EOL;   // Notice: Undefined variable: ageyears
+
+  // 波括弧で変数名を囲む
+  echo "${age}years old.", PHP_EOL; // 15years old
+?>
+<h3>ヒアドキュメント</h3>
+<?php
+  $age = 15;
+
+  $foo = <<<EOI
+ヒアドキュメントでは、このように、
+複数行にわたる文章をそのまま表記することができます。
+
+Tomの年齢は "$age" 歳です。
+EOI;
+  echo $foo;
+?>
+
+<h3>文字列型とキャスト</h3>
+<?php
+  echo 15.0;  // 文字列型にキャストされ、整数と判断され、".0"が省略される
+  printf('%.1f', 15.0);
+?>
