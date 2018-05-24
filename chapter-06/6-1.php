1f76d7a4bcb7db044289be8bc3c523311fee028c
ueki05 <uekiyuta1991@gmail.com>
2018-05-24 23:39:34 +0900

    6.1.2 HTMLとフォーム

--- chapter-06/6-1.php
+++ chapter-06/6-1.php
@@ -5,4 +5,15 @@
   <li>リクエスト</li>
   <li>レスポンス</li>
 </ul>
+<h2>6.1.2 HTMLとフォーム</h2>
+<?php $hour = date('H'); // 現在の時を$hour変数に格納 ?>
+
+<?php if (5 <= $hour && $hour < 10): ?>
+<p>Good morning</p>
+<?php elseif (10 <= $hour && $hour < 18): ?>
+<p>Hello</p>
+<?php else: ?>
+<p>Good evening</p>
+<?php endif ?>
+<p>It's <?php echo $hour; ?> o'clock now.</p>
 
