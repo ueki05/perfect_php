08e2336b3f627800a929c924503e84cd752f6969
ueki05 <uekiyuta1991@gmail.com>
2018-05-27 15:18:53 +0900

    6.2.5 コメント投稿フォームの作成

---
+++ chapter-06/oneline_bbs/bbs.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>ひとこと掲示板</title>
+</head>
+<body>
+  <h2>ひとこと掲示板</h2>
+
+  <form action="bbs.php" method="post">
+    名前: <input type="text" name="name"><br>
+    ひとこと: <input type="text" name="comment" size=60"><br>
+    <input type="submit" name="submit" value="送信">
+  </form>
+</body>
+</html>
