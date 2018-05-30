19cd4a71957d986082aa8dca4bab52ec7b114305
ueki05 <uekiyuta1991@gmail.com>
2018-05-30 23:48:08 +0900

    6.2.9 投稿後のリダイレクト

--- chapter-06/oneline_bbs/bbs.php
+++ chapter-06/oneline_bbs/bbs.php
@@ -42,6 +42,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // 保存する
     mysql_query($sql, $link);
+
+    mysql_close($link);
+
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
   }
 }
 
