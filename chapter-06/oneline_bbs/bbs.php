dc0283ca4015e1f1199c8d9bc2a4a44e972fe570
ueki05 <uekiyuta1991@gmail.com>
2018-06-01 00:54:25 +0900

    6.3.2 ロジックとビューの分離

--- chapter-06/oneline_bbs/bbs.php
+++ chapter-06/oneline_bbs/bbs.php
@@ -50,6 +50,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 ?>
+<?php
+// 投稿された内容を取得するSQLを作成して結果を取得
+$sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
+$result = mysql_query($sql, $link);
+
+// 取得した結果を$postsに格納
+$post = array();
+if ($result !== false && mysql_num_rows($result)) {
+  while ($post = mysql_fetch_assoc($result)) {
+    $posts[] = $post;
+  }
+}
+
+// 取得結果を解放して接続を閉じる
+mysql_free_result($result);
+mysql_close($link);
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -72,27 +91,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     ひとこと: <input type="text" name="comment" size=60"><br>
     <input type="submit" name="submit" value="送信">
   </form>
-  <?php
-  // 投稿された内容を取得するSQLを作成して結果を取得
-  $sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
-  $result = mysql_query($sql, $link);
-  ?>
-  <?php if ($result !== false && mysql_num_rows($result)): ?>
+  <?php if (count($posts) > 0): ?>
   <ul>
-    <?php while ($post = mysql_fetch_assoc($result)): ?>
+    <?php foreach ($posts as $post): ?>
     <li>
       <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>
       <?php echo htmlspecialchars($post['comment'], ENT_QUOTES, 'UTF-8'); ?>
       - <?php echo htmlspecialchars($post['created_at'], ENT_QUOTES, 'UTF-8'); ?>
     </li>
-    <?php endwhile; ?>
+    <?php endforeach; ?>
   </ul>
   <?php endif; ?>
-
-  <?php
-  // 取得結果を解放して接続を閉じる
-  mysql_free_result($result);
-  mysql_close($link);
-  ?>
 </body>
 </html>
