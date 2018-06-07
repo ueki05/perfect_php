3c5d5b834eacde44e8203346b9c2589b039609b4
ueki05 <uekiyuta1991@gmail.com>
2018-06-07 23:00:28 +0900

    13.1.2 変数をフォーマットする

--- chapter-13/13-1.php
+++ chapter-13/13-1.php
@@ -2,3 +2,16 @@
 <h2>13.1.1 文字列に対する操作</h2>
 <a href="http://jp.php.net/manual/ja/book.strings.php" target="_blank">公式マニュアル</a>
 
+<h2>13.1.2 変数をフォーマット</h2>
+<?php
+$index = 125;
+$message = '出力を行います';
+printf("[%10d] %s", $index, $message);
+?>
+
+<?php
+$a = 20;
+$formatted = sprintf("%+010d", $a);
+echo $formatted;
+?>
+
